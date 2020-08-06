8055e41f0d7bf6e3f3904577269d0cb262ba3372
Moses <gabe.susman@gmail.com>
2020-08-06 13:36:27 -0600

    first commit for a dev exercise I'm performing

---
+++ PassingData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by Moses on 8/6/20.
+//  Copyright © 2020 Max Susman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
